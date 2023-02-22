dc59076e61cac718587d8b53318d7a81eb2033ae
AndreaSciscioli <113891464+AndreaSciscioli@users.noreply.github.com>
2023-02-22 13:26:45 +0100

    custom query2

---
+++ src/main/java/co/develhope/customqueries2/repositories/FlightRepository.java
@@ -0,0 +1,17 @@
+package co.develhope.customqueries2.repositories;
+
+import co.develhope.customqueries2.entities.Flight;
+import co.develhope.customqueries2.entities.FlightStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository     
+public interface FlightRepository extends JpaRepository<Flight, Long> {
+    List<Flight> getCustomFlight(FlightStatus p1, FlightStatus p2);
+
+    Page<Flight> findAllByStatus(FlightStatus status, PageRequest of);
+}
